dfab036b987581296fcc1ac6247e23076c1903ca
111Bond <joaoprp111@gmail.com>
2021-01-22 12:29:13 +0000

    Cliente com writeworker

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 12345);
+
+            Thread t1 = new Thread(new WriteWorker(socket));
+            //Thread t2 = new Thread(new ReadWorker(socket)); <-- Ainda nao implementado
+
+            t1.start();
+            //t2.start();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/WriteWorker.java
+++ src/WriteWorker.java
@@ -1,2 +1,30 @@
-public class WriterWorker {
+import java.io.*;
+import java.net.Socket;
+
+public class WriteWorker implements Runnable {
+    private Socket s;
+
+    public WriteWorker(Socket s){
+        this.s = s;
+    }
+
+    public void run(){
+        try {
+            DataOutputStream out = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
+            BufferedReader systemin = new BufferedReader(new InputStreamReader(System.in));
+
+            String userInput;
+
+            while((userInput =  systemin.readLine()) != null){
+                out.writeUTF(userInput); //Username
+                out.writeUTF(userInput); //Password
+                out.flush();
+            }
+            s.shutdownOutput();
+            s.shutdownInput();
+            s.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
